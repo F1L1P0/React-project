b63ea240b45103d59093bd30fb7e9911d2eed44e
F1L1P0 <drabekfilip@seznam.cz>
2021-04-22 07:54:19 +0200

    adding about page as a test

--- urbanek-web/src/App.js
+++ urbanek-web/src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import Header from "./components/Header.js";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
+import About from "./components/About";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Route, Link} from "react-router-dom"; 
 
@@ -12,6 +13,7 @@ function App() {
       <Header/>
       <Route exact path="/" component={Home}/>
       <Route exact path="/Contact" component={Contact}/>
+      <Route exact path="/About" component={About}/>
     </div>
   );
 }
---
+++ urbanek-web/src/components/About.js
@@ -0,0 +1,15 @@
+import react from "react";
+
+function About() {
+    return(
+        <section className="mb-2">
+            <div className="container">
+                <div className="row">
+                    about
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default About;
