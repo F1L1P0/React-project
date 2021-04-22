9e678bd5475871019f8859d596160962ee76440b
F1L1P0 <39796297+F1L1P0@users.noreply.github.com>
2021-04-22 07:12:31 +0200

    Add files via upload

---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import react from "react";
+
+function Contact() {
+    return(
+        <section className="mb-2">
+            <div className="container">
+                <div className="row">
+                    Kontakty
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Contact;
