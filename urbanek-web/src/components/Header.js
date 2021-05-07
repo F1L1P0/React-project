1b568743e5530de52eee7e20882fbbd3cbf25862
F1L1P0 <drabekfilip@seznam.cz>
2021-05-07 14:25:10 +0200

    bazeny web

--- urbanek-web/src/components/Header.js
+++ urbanek-web/src/components/Header.js
@@ -1,22 +1,35 @@
 
-import React from "react";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const Header = () => 
-<section className="mb-2">
-    <div className="container">
-        <div className="row justify-content-between BO-underline">
-            <div className="col-4 d-flex align-items-center">
-                <a href="mailto:eshop@bana.cz" className="BO-navigation">eshop@bana.cz</a>
-            </div>
-            <div className="col-8 d-flex justify-content-end">
-                <a className="p-2 BO-navigation" href="./">Úvod</a>
-                <a className="p-2 BO-navigation">O nás</a>
-                <a className="p-2 BO-navigation">Přihlášení</a>
-                <a className="p-2 BO-navigation">Registrace</a>
-                <a className="p-2 BO-navigation" href="./Contact">Kontakt</a>
-            </div>
-        </div>
-    </div>
-</section>
+<div className="app">
 
+   <div className="bg-header">
+        <div className="wing">
+            <section className="mb-2">
+                <div className="container">
+                    <div className="row justify-content-between BO-underline">
+                        <div className="col-4 d-flex align-items-center">
+                            <a href="mailto:eshop@bana.cz" className="BO-navigation">eshop@bana.cz</a>
+                        </div>
+                        <div className="col-8 d-flex justify-content-end">
+                            <a className="p-2 BO-navigation" href="./">Úvod</a>
+                            <a className="p-2 BO-navigation">O nás</a>
+                            <a className="p-2 BO-navigation">Přihlášení</a>
+                            <a className="p-2 BO-navigation">Registrace</a>
+                            <a className="p-2 BO-navigation" href="./Contact">Kontakt</a>
+                        </div>
+                    </div>
+                </div>
+                    <div className="btns">
+                        <a className="nav-btn" href="#link1"></a>
+                    </div>
+            </section>
+        </div>
+   </div>
+            <section id="link1">
+                
+            </section>
+</div>
 export default Header;
--- urbanek-web/src/components/Home.js
+++ urbanek-web/src/components/Home.js
@@ -1,13 +1,28 @@
 
-import React from "react";
 
-const Home = () => 
+/* const Home = () => 
 <section className="mb-2">
     <div className="container">
         <div className="row justify-content-between">
-            Doma
+            Home
         </div>
     </div>
-</section>
+</section> */
+
+import React, { useState } from 'react';
+
+function Home() {
+  // Declare a new state variable, which we'll call "count"  
+  //   const [count, setCount] = useState(0);
+  // return (
+  //   <div>
+  //     <p>You clicked {count} times</p>
+  //     <button onClick={() => setCount(count + 1)}>
+  //       Click me
+  //     </button>
+  //   </div>
+  // );
+  return true;
+}
 
 export default Home;
